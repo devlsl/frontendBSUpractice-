49f7eccc51b03841072b9e35f8e71f72d41d6ac3
devlsl <vslozhen@gmail.com>
2023-06-25 20:52:48 +0300

    fix add new application button

--- src/service/addNewApplication.ts
+++ src/service/addNewApplication.ts
@@ -3,17 +3,6 @@ import axios from 'axios'
 import { AuthorizedUser } from 'data/types'
 
 export async function addNewApplication(user: AuthorizedUser, items: number[]) {
-  const query = url + 'user'
-  const response = await axios.post(query, { params: { user, items } })
-
-  return response.data
-
-  if (user) {
-    return { checkedUser: user, successMessage: response.data.message } as {
-      checkedUser: AuthorizedUser
-      successMessage: string
-    }
-  }
-
-  throw new Error(response.data.message)
+  const query = url + 'client'
+  await axios.post(query, { params: { user, items } })
 }
